df4a063471f97c689971d03ad2a9e2c572a9845f
Adam J. Jackson <a.j.jackson@physics.org>
2026-04-15 17:23:35 +0100

    Update validation workflow

--- dev/validation/workflow/scripts/write_isotropic_fundamentals.py
+++ dev/validation/workflow/scripts/write_isotropic_fundamentals.py
@@ -3,11 +3,9 @@ import numpy as np
 from snakemake.script import snakemake
 
 from abinslib.bose import BoseOccupation
-from abinslib.isotropic_incoherent import (
-    q_scaling_isotropic_incoherent_spectra,
-    calculate_mode_displacements,
-    calculate_atomic_displacements,
-)
+from abinslib.displacements import Displacements
+from abinslib.isotropic_incoherent import q_scaling_isotropic_incoherent_spectra
+
 from abinslib.util import calculate_indirect_q2
 
 modes = QpointPhononModes.from_json_file(snakemake.input[0])
@@ -16,20 +14,17 @@ temperature = Quantity(float(mantid_data.metadata["temperature"]), "kelvin")
 
 bins = mantid_data.get_bin_edges(restrict_range=False)
 
-b = calculate_mode_displacements(
-    modes, temperature=temperature, occupation=BoseOccupation.N_PLUS_ONE
-)
-a = calculate_atomic_displacements(
-    modes,
-    temperature=temperature,
-)
+displacements = Displacements.from_modes(modes, temperature=temperature)
+dw = displacements.to_atomic_displacements()
+
 q2 = calculate_indirect_q2(
     mantid_data.get_bin_centres(),
     angle=(134.98885653282196 * np.pi / 180),
     final_energy=Quantity(32.0, "cm_1").to("hartree"),
 )
 
-spectra = q_scaling_isotropic_incoherent_spectra(modes, b, a, q2, bins)
+spectra = q_scaling_isotropic_incoherent_spectra(
+    modes, displacements, dw, q2, bins)
 spectrum = spectra.sum()
 
 spectrum.to_json_file(snakemake.output[0])
