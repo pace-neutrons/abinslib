b9eaf5b634ef5ddcdc0feef7d232bc338d424fc2
Adam J. Jackson <a.j.jackson@physics.org>
2026-07-08 14:05:13 +0100

    Make pooch optional dependency, test fallback error
    For full coverage check, test suite should be run with the "tutorial"
    extra and will include reference file download. But for normal test
    runs this can be ommitted and that test will skip safely.

--- src/abinslib/data.py
+++ src/abinslib/data.py
@@ -1,32 +1,54 @@
 """Wrapper to fetch reference data for tutorials etc."""
 
+from __future__ import annotations
+
 from pathlib import Path
+from typing import TYPE_CHECKING
 
-import pooch
+if TYPE_CHECKING:
+    from pooch import Pooch
 
-_EUPHONIC_TEST_DATA = pooch.create(
-    path=pooch.os_cache("abinslib"),
-    # Currently this location is overridden by the only file in registry.
-    # Later it will point to something like github release or Zenodo archive,
-    # where most ref data can be kept in flat structure with short names.
-    base_url=(
-        "https://github.com/pace-neutrons/Euphonic/raw/"
-        "master/tests_and_analysis/test/data/"
-    ),
-    registry={
-        "NaH.phonon": (
-            "ccb30647b5cc9a2f3ab470dda77bc6f3ccc19cb1b8adaf35f5c40ccdaabccde1"
-        )
-    },
-    urls={
-        "NaH.phonon": (
-            "https://github.com/pace-neutrons/Euphonic/raw/master/"
-            "tests_and_analysis/test/data/castep_files/NaH/NaH.phonon"
-        )
-    },
-)
+
+def _setup_ref_data() -> Pooch:
+    return pooch.create(
+        path=pooch.os_cache("abinslib"),
+        # Currently this location is overridden by the only file in registry.
+        # Later it will point to something like github release or Zenodo archive,
+        # where most ref data can be kept in flat structure with short names.
+        base_url=(
+            "https://github.com/pace-neutrons/Euphonic/raw/"
+            "master/tests_and_analysis/test/data/"
+        ),
+        registry={
+            "NaH.phonon": (
+                "ccb30647b5cc9a2f3ab470dda77bc6f3ccc19cb1b8adaf35f5c40ccdaabccde1"
+            )
+        },
+        urls={
+            "NaH.phonon": (
+                "https://github.com/pace-neutrons/Euphonic/raw/master/"
+                "tests_and_analysis/test/data/castep_files/NaH/NaH.phonon"
+            )
+        },
+    )
 
 
 def get_data(filename: str) -> Path:
     """Get external reference data by filename."""
+    if _EUPHONIC_TEST_DATA is None:
+        msg = (
+            "Could not construct reference data collection. Ensure 'pooch' was"
+            " installed, e.g. with 'pip install abinslib[tutorials]'."
+            )
+        raise ImportError(msg)
+
     return Path(_EUPHONIC_TEST_DATA.fetch(filename))
+
+
+try:
+    import pooch  # noqa: F401
+
+    _EUPHONIC_TEST_DATA: Pooch | None = _setup_ref_data()
+
+except ImportError:
+    _EUPHONIC_TEST_DATA = None
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,9 +1,14 @@
 from pathlib import Path
 
-from abinslib.data import get_data
+import pytest
+
+import abinslib.data
+
 
 def test_get_data():
-    nah = get_data("NaH.phonon")
+    pytest.importorskip("pooch")
+
+    nah = abinslib.data.get_data("NaH.phonon")
     assert isinstance(nah, Path)
     assert nah.is_file()
     assert nah.name == "NaH.phonon"
@@ -11,3 +16,10 @@ def test_get_data():
     with nah.open() as fd:
         header = next(fd).strip()
         assert header == "BEGIN header"
+
+
+def test_missing_pooch_error(monkeypatch):
+    monkeypatch.setattr(abinslib.data, "_EUPHONIC_TEST_DATA", None)
+
+    with pytest.raises(ImportError, match="pooch"):
+        abinslib.data.get_data("NaH.phonon")
