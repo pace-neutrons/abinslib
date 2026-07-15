36fbb12f30153506318fcdff5adeceffa64684dc
Adam J. Jackson <a.j.jackson@physics.org>
2026-07-15 16:46:18 +0100

    Improve website colours
    Light/dark behaviour is improved a bit and Web Accessibility
    Evaluation Tool seems happy enough.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,9 +31,52 @@ exclude_patterns = ["tutorials/GALLERY_HEADER.rst"]
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+_PALE = "#dae9e0"
+_DESATURATED = "#97b1ab"
+_BRIGHT = "#c4fcf0"
+_LIGHT_GREEN = "#4eae9e"
+_GREEN = "#00796b"
+_TURQUOISE = "#007e8c"
+_DARK_GREEN = "#344b47"
+_DARKEST = "#00392f"
+
 html_theme = "furo"
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
+html_theme_options = {
+    "light_css_variables": {
+        "color-brand-primary": _GREEN,
+        "color-brand-content": _GREEN,
+        "color-brand-visited": _DESATURATED,
+        "color-link": _GREEN,
+        "color-link--visited": _TURQUOISE,
+        "color-link--hover": _DESATURATED,
+        "color-admonition-title--note": _LIGHT_GREEN,
+        "color-admonition-title-background--note": _BRIGHT,
+        "color-api-keyword": _DARK_GREEN,
+        "color-api-pre-name": _GREEN,
+        "color-api-name": _DARK_GREEN,
+        # The next three tags are not built-ins, but referred to as variables
+        # in _static/custom.css; this gets the auto dark/light mode working.
+        "color-download-button-bg": _PALE,
+        "color-download-gradient-top": _PALE,
+        "color-download-gradient-bottom": _LIGHT_GREEN,
+    },
+    "dark_css_variables": {
+        "color-brand-primary": _LIGHT_GREEN,
+        "color-brand-content": _LIGHT_GREEN,
+        "color-brand-visited": _TURQUOISE,
+        "color-link": _LIGHT_GREEN,
+        "color-link--visited": _GREEN,
+        "color-link--hover": _TURQUOISE,
+        "color-admonition-title--note": _LIGHT_GREEN,
+        "color-admonition-title-background--note": _DARK_GREEN,
+        "color-admonition-background": _DARKEST,
+        "color-download-button-bg": _DESATURATED,
+        "color-download-gradient-top": _LIGHT_GREEN,
+        "color-download-gradient-bottom": _GREEN,
+    },
+}
 
 # -- AutoAPI configuration ---------------------------------------------------
 autoapi_type = "python"
